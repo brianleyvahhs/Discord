104f209d9c3dc3c0c37c9ed468d33d3e108dbe39
Brian Leyva Contreras <57155914+brianleyvahhs@users.noreply.github.com>
2020-12-07 09:33:55 -0800

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,1 +1,12 @@
+Discord = require('discord.js');
+Bot = new Discord.Client();
+Prefix = ("Enter Prefix Here"); // Your Personal Bot Prefix //
+Token = ("Enter Token Here"); // Your Personal Bot Token //
+Status = ("Online");
 
+Bot.once('ready', () => {
+  console.info(`Name: ${Bot.user.username}`);
+  console.info(`Bot Status: ${Status}!`);
+});
+
+Bot.login(Token);
